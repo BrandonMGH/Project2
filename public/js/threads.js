b3eeefe5d147c5e293b4553e39887f5971812310
jermainehunter <jermainehunter@gmail.com>
2019-04-10 17:04:27 -0700

    made comments in threads.js file

--- public/js/threads.js
+++ public/js/threads.js
@@ -1,3 +1,6 @@
+
+//hides/shows create thread button on threads page JH 4/10/19
+//no touchy
 $("#create_topic_but").click(function () {
     $("#create_topic").show();
     $("#create_topic_but").hide();
