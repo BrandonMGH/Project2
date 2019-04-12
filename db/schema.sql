6e87d657c27e9390e17f9de815d65a40122ffc6f
Brandon Harris <BrandonMGH@Yahoo.com>
2019-04-11 18:36:17 -0700

    css image and fooder changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,13 @@
 DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE e7cj6qh5vzj6xt6u;
 
-USE exampledb;
+USE e7cj6qh5vzj6xt6u;
+
+SELECT * from Posts;
+SELECT * from Threads; 
+SELECT * from Topics;
+SELECT * from Users; 
+
 
 INSERT INTO Users (user_name, email, password, createdAt, updatedAt) VALUES ("Hi", "Hi@Email.com", "Hiagain", "1000-01-01 00:00:00", "1000-01-01 00:00:00");
 INSERT INTO Topics (createdAt, updatedAt) VALUES ("1000-01-01 00:00:00", "1000-01-01 00:00:00");
@@ -11,15 +17,19 @@ INSERT INTO Topics (createdAt, updatedAt) VALUES ("1000-01-01 00:00:00", "1000-0
 INSERT INTO Topics (createdAt, updatedAt) VALUES ("1000-01-01 00:00:00", "1000-01-01 00:00:00");
 INSERT INTO Topics (createdAt, updatedAt) VALUES ("1000-01-01 00:00:00", "1000-01-01 00:00:00");
 
-INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Thread 1", 1, 1, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
-INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Thread 2", 1, 2, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
-INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Thread 3", 1, 3, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
-INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Thread 4", 1, 4, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
-INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Thread 5", 1, 5, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
-INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Thread 6", 1, 6, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
+INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Welcome to the Github Thread", 1, 1, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
+INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Welcome to the Bootstrap Thread ", 1, 2, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
+INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Welcome to the Node Thread", 1, 3, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
+INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Welcome to the Jquery Thread", 1, 4, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
+INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Welcome to the MySQL Thread", 1, 5, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
+INSERT INTO Threads (title, UserId, TopicId, createdAt, updatedAt ) VALUES ("Welcome to the Javascript Thread", 1, 6, "1000-01-01 00:00:00" ,"1000-01-01 00:00:00" );
 
-INSERT INTO Posts (title, body, createdAt, updatedAt, ThreadId, TopicId, UserId) VALUES ("Post 1", "Body 1", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 1, 1, 1);
-INSERT INTO Posts (title, body, createdAt, updatedAt, ThreadId, TopicId, UserId) VALUES ("Post 2", "Body 2", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 1, 1, 1);
+INSERT INTO Posts ( body, createdAt, updatedAt, ThreadId, UserId) VALUES ("Welcome to the Github thread page!  This thread page is dedicated to all topics of interest concerning Github.  If you would like to create a new conversation, please feel free to make a new thread on the threads page.  If you would rather contribute to an existing conversation, please click on any of the existing threads on the Github thread page.  Additional functionality coming soon!", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 8, 1);
+INSERT INTO Posts ( body, createdAt, updatedAt, ThreadId, UserId) VALUES ( "Welcome to the Bootstrap thread page!  This thread page is dedicated to all topics of interest concerning Bootstrap.  If you would like to create a new conversation, please feel free to make a new thread on the threads page.  If you would rather contribute to an existing conversation, please click on any of the existing threads on the Bootstrap thread page.  Additional functionality coming soon!", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 9, 1);
+INSERT INTO Posts ( body, createdAt, updatedAt, ThreadId, UserId) VALUES ( "Welcome to the Node.JS thread page!  This thread page is dedicated to all topics of interest concerning Node.JS.  If you would like to create a new conversation, please feel free to make a new thread on the threads page.  If you would rather contribute to an existing conversation, please click on any of the existing threads on the Node.JS thread page.  Additional functionality coming soon!", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 10, 1);
+INSERT INTO Posts ( body, createdAt, updatedAt, ThreadId, UserId) VALUES ( "Welcome to the Jquery thread page!  This thread page is dedicated to all topics of interest concerning Jquery.  If you would like to create a new conversation, please feel free to make a new thread on the threads page.  If you would rather contribute to an existing conversation, please click on any of the existing threads on the Jquery thread page.  Additional functionality coming soon!", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 11, 1);
+INSERT INTO Posts ( body, createdAt, updatedAt, ThreadId, UserId) VALUES ( "Welcome to the MySQL thread page!  This thread page is dedicated to all topics of interest concerning MySQL.  If you would like to create a new conversation, please feel free to make a new thread on the threads page.  If you would rather contribute to an existing conversation, please click on any of the existing threads on the MySQL thread page.  Additional functionality coming soon!", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 12, 1);
+INSERT INTO Posts ( body, createdAt, updatedAt, ThreadId, UserId) VALUES ( "Welcome to the Javascript thread page!  This thread page is dedicated to all topics of interest concerning Javascript.  If you would like to create a new conversation, please feel free to make a new thread on the threads page.  If you would rather contribute to an existing conversation, please click on any of the existing threads on the Javascript thread page.  Additional functionality coming soon!", "1000-01-01 00:00:00", "1000-01-01 00:00:00", 13, 1);
 
 -- DROP DATABASE IF EXISTS testdb;
 -- CREATE DATABASE testdb;
