6572dd9466c7524072636f2ed8be891a14601264
Brandon Harris <BrandonMGH@Yahoo.com>
2019-04-11 18:21:09 -0700

    update

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     const Post = sequelize.define("Post", {
         body: {
             type: DataTypes.TEXT,
-            allowNull: false,
+            // allowNull: false,
         }
     });
 
--- public/js/index.js
+++ public/js/index.js
@@ -127,3 +127,36 @@ $exampleList.on("click", ".delete", handleDeleteBtnClick);
 //     }
 //   );
 // });
+
+const topicLogo = {
+  GitHub: "/images/github.png",
+  
+  Bootstrap: "/images/bootstrap.png",
+
+  Node: "/images/node.png",
+
+  jQuery: "/images/jquery.png",
+
+  MySQL: "/images/mysql.png",
+
+  JavaScript: "/images/javascript.png",
+  
+};
+
+$(document).ready(function() {
+  const $topic = $("#topic_header");
+  const title = $topic.attr("data-id");
+  console.log(title);
+  console.log(topicLogo[title]);
+  const $image = $("<img>").attr({
+    src: `${topicLogo[title]}`,
+    // src: "/images/github.png",
+    alt: "Topic Logo",
+    title: "Topic Logo",
+    width: 40,
+    height: 40,
+    class: "topic-image",
+  
+  })
+  $image.appendTo("#topic_header");
+});
